36f2902f96625eacdcc1d654f5d380a3928f4183
Martin Cabrera <martin@kisland.com>
2018-08-23 20:24:40 +0200

    adds seeds loading initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Activity.destroy_all
+Rake::Task['import:json_file'].invoke
