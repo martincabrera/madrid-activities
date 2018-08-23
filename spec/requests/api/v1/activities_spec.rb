80e88437ee65c3d5cb548668583b0d7132aff881
Martin Cabrera <martin@kisland.com>
2018-08-23 23:41:53 +0200

    adds more specs to Activities Request specs

--- spec/requests/api/v1/activities_spec.rb
+++ spec/requests/api/v1/activities_spec.rb
@@ -1,7 +1,8 @@
-describe "Activities Endpoint" do
-  it 'sends a list of activities when there are activities' do
+# frozen_string_literal: true
 
-    names = %w(rastro, retiro, sol, callao)
+describe 'Activities Endpoint' do
+  it 'sends a list of activities when there are activities' do
+    names = %w[rastro retiro sol callao]
     names.map do |name|
       create(:activity, name: name)
     end
@@ -15,8 +16,7 @@ describe "Activities Endpoint" do
     expect(json['features'].length).to eq(names.count)
   end
 
-    it 'sends an empty list of activities when there are NO activities' do
-
+  it 'sends an empty list of activities when there are NO activities' do
     get '/api/v1/activities'
 
     json = JSON.parse(response.body)
@@ -24,9 +24,72 @@ describe "Activities Endpoint" do
     expect(response).to have_http_status(200)
 
     expect(json['features'].length).to be_zero
+  end
+
+  context 'when filtering' do
+    before do
+      create(:activity, category: 'shopping', district: 'Centro', location: 'outdoors')
+      create(:activity, category: 'cultural', district: 'Latina', location: 'indoors')
     end
 
-  context 'when filtering with params' do
-    it 'returns the right '
+    it 'returns the right activity by category' do
+      get '/api/v1/activities', params: { category: 'shopping' }
+
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+
+      expect(json['features'].length).to eq(1)
+    end
+
+    it 'does not return activities by an unknown category' do
+      get '/api/v1/activities', params: { category: 'NOT-shopping' }
+
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+
+      expect(json['features'].length).to be_zero
+    end
+
+    it 'returns the right activity by district' do
+      get '/api/v1/activities', params: { district: 'Centro' }
+
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+
+      expect(json['features'].length).to eq(1)
+    end
+
+    it 'does not return activities by an unknown district' do
+      get '/api/v1/activities', params: { district: 'NOT-Centro' }
+
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+
+      expect(json['features'].length).to be_zero
+    end
+
+    it 'returns the right activity by location' do
+      get '/api/v1/activities', params: { location: 'outdoors' }
+
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+
+      expect(json['features'].length).to eq(1)
+    end
+
+    it 'does not return activities by an unknown location' do
+      get '/api/v1/activities', params: { location: 'NOT-outdoors' }
+
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+
+      expect(json['features'].length).to be_zero
+    end
   end
 end
