7303d90f6dc3961bfbac011d79ec06c15dadd01d
Martin Cabrera <martin@kisland.com>
2018-08-28 16:05:20 +0200

    adds more use cases

--- spec/lib/activity_prescriptor_spec.rb
+++ spec/lib/activity_prescriptor_spec.rb
@@ -41,7 +41,7 @@ describe ActivityPrescriptor do
     end
   end
 
-  context 'when there are now activities with opening hours in users time range' do
+  context 'when there are no activities with opening hours in users time range' do
     before do
       create(:opening_hour, activity: first_activity, start_hour: TimeFormatter.hours_minutes_to_seconds('16:00'),
                             end_hour: TimeFormatter.hours_minutes_to_seconds('20:00'), day_of_the_week: 'mo')
@@ -65,6 +65,34 @@ describe ActivityPrescriptor do
 
     it 'returns nothing' do
       params = { start_hour: '19:00', end_hour: '20:30', day_of_the_week: 'mo', category: 'cultural' }
+      expect(described_class.new(params).recommend).to be_nil
+    end
+  end
+
+  context 'when there is 1 activity with 2 opening hours in users time range' do
+    before do
+      create(:opening_hour, activity: first_activity, start_hour: TimeFormatter.hours_minutes_to_seconds('10:00'),
+                            end_hour: TimeFormatter.hours_minutes_to_seconds('14:00'), day_of_the_week: 'mo')
+      create(:opening_hour, activity: first_activity, start_hour: TimeFormatter.hours_minutes_to_seconds('16:00'),
+                            end_hour: TimeFormatter.hours_minutes_to_seconds('20:00'), day_of_the_week: 'mo')
+    end
+
+    it 'returns the activity' do
+      params = { start_hour: '10:00', end_hour: '20:00', day_of_the_week: 'mo', category: 'shopping' }
+      expect(described_class.new(params).recommend).to eq first_activity
+    end
+  end
+
+  context 'when there is 1 activity with 2 opening hours in users time range but with not enough time to complete the visit' do
+    before do
+      create(:opening_hour, activity: first_activity, start_hour: TimeFormatter.hours_minutes_to_seconds('10:00'),
+                            end_hour: TimeFormatter.hours_minutes_to_seconds('14:00'), day_of_the_week: 'mo')
+      create(:opening_hour, activity: first_activity, start_hour: TimeFormatter.hours_minutes_to_seconds('16:00'),
+                            end_hour: TimeFormatter.hours_minutes_to_seconds('20:00'), day_of_the_week: 'mo')
+    end
+
+    it 'returns nothing' do
+      params = { start_hour: '13:00', end_hour: '17:00', day_of_the_week: 'mo', category: 'shopping' }
       expect(described_class.new(params).recommend).to be_nil
     end
   end
